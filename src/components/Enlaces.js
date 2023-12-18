5b5f83306835b3cf8c6a82c42eb9693f3786b7fa
Jackson <armnellgomez@gmail.com>
2023-12-18 04:01:17 -0500

    lista para ingresar contenido

---
+++ src/components/Enlaces.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import '../styles/Enlaces.css';
+
+function Enlaces() {
+	const [selectedTab, setSelectedTab] = useState('Proyectos');
+
+	// const renderTab = () => {
+	//     switch (selectedTab) {
+	//         case 'Proyectos':
+	//             return <Proyectos />;
+	//         case '¿Quien Soy?':
+	//             return <QuienSoy />;
+	//         case 'Contacto':
+	//             return <Contacto />;
+	//         default:
+	//             return <Proyectos />;
+	//     }
+	// };
+
+	return (
+		<>
+			<ul className="nav nav-pills nav-fill small" id="pillNav2" role="tablist" style={{ "--bs-nav-link-color": "var(--tono-1)", "--bs-nav-pills-link-active-color": "#000000", "--bs-nav-pills-link-active-bg": "var(--tono-1)" }}>
+				<li className="nav-item" role="presentation">
+					<button className={`nav-link ${selectedTab === 'Proyectos' ? 'active' : ''}`} id="home-tab2" onClick={() => setSelectedTab('Proyectos')} type="button" role="tab" aria-selected="true">Seccion 1</button>
+				</li>
+				<li className="nav-item" role="presentation">
+					<button className={`nav-link ${selectedTab === '¿Quien Soy?' ? 'active' : ''}`} id="profile-tab2" onClick={() => setSelectedTab('¿Quien Soy?')} type="button" role="tab" aria-selected="false">Seccion 2</button>
+				</li>
+				<li className="nav-item" role="presentation">
+					<button className={`nav-link ${selectedTab === 'Contacto' ? 'active' : ''}`} id="contact-tab2" onClick={() => setSelectedTab('Contacto')} type="button" role="tab" aria-selected="false">Seccion 3</button>
+				</li>
+			</ul>
+			{/* {renderTab()} */}
+		</>
+	)
+}
+
+export default Enlaces;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import Logo from '../img;
+import '../styles/Header.css';
+
+function Header() {
+	return (
+		<>
+			<header id="inicio">
+				<nav className="navbar navbar-expand-lg bg-transparent">
+					<div className="container-fluid">
+						<a className="navbar-brand" href="/">
+							<a class="navbar-brand fs-1" href="/">AM</a>
+							{/* <img src={Logo} alt="logo AG" width="36" /> */}
+						</a>
+						<section className="social-header">
+							<a href="https://www.instagram.com/newkarlahg?igshid=YTQwZjQ0NmI0OA==" target="_blank" rel="noreferrer">
+								<i className="bi bi-instagram" style={{ color: '#000000' }}></i>
+							</a>
+						</section>
+					</div>
+				</nav>
+
+				{/* <section>
+					<h1>Una Amiga Manicurista</h1>
+					<h2>NAIL ART</h2>
+				</section> */}
+			</header>
+		</>
+	)
+}
+
+export default Header;
