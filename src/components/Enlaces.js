d0adc98ce568da7b09fe2700febfc09035dfe627
Jackson <armnellgomez@gmail.com>
2023-12-20 21:35:35 -0500

    Cambie texto en el componente enlaces.

--- src/components/Enlaces.js
+++ src/components/Enlaces.js
@@ -21,13 +21,13 @@ function Enlaces() {
 		<>
 			<ul className="nav nav-pills nav-fill small" id="pillNav2" role="tablist" style={{ "--bs-nav-link-color": "var(--tono-1)", "--bs-nav-pills-link-active-color": "#000000", "--bs-nav-pills-link-active-bg": "var(--tono-1)" }}>
 				<li className="nav-item" role="presentation">
-					<button className={`nav-link ${selectedTab === 'Proyectos' ? 'active' : ''}`} id="home-tab2" onClick={() => setSelectedTab('Proyectos')} type="button" role="tab" aria-selected="true">Seccion 1</button>
+					<button className={`nav-link ${selectedTab === 'Proyectos' ? 'active' : ''}`} id="home-tab2" onClick={() => setSelectedTab('Proyectos')} type="button" role="tab" aria-selected="true">Blog</button>
 				</li>
 				<li className="nav-item" role="presentation">
-					<button className={`nav-link ${selectedTab === '¿Quien Soy?' ? 'active' : ''}`} id="profile-tab2" onClick={() => setSelectedTab('¿Quien Soy?')} type="button" role="tab" aria-selected="false">Seccion 2</button>
+					<button className={`nav-link ${selectedTab === '¿Quien Soy?' ? 'active' : ''}`} id="profile-tab2" onClick={() => setSelectedTab('¿Quien Soy?')} type="button" role="tab" aria-selected="false">Nail Art</button>
 				</li>
 				<li className="nav-item" role="presentation">
-					<button className={`nav-link ${selectedTab === 'Contacto' ? 'active' : ''}`} id="contact-tab2" onClick={() => setSelectedTab('Contacto')} type="button" role="tab" aria-selected="false">Seccion 3</button>
+					<button className={`nav-link ${selectedTab === 'Contacto' ? 'active' : ''}`} id="contact-tab2" onClick={() => setSelectedTab('Contacto')} type="button" role="tab" aria-selected="false">Tienda</button>
 				</li>
 			</ul>
 			{/* {renderTab()} */}
